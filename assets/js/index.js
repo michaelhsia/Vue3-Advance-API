9b536421683180b2f8bdaa8c2df7bb912bbab155
michaelhsia <michaelhsia8716@gmail.com>
2024-01-31 16:47:36 +0800

    finished veevalidate

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,137 @@
-import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+// vue esm
+// 用 esm 方式載入 Vue 會 VeeValidate 出錯，因此使用一般 CDN
+// import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+// vue-loading
+import { LoadingPlugin } from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
+
+// bs5
+import * as bootstrap from "bootstrap/dist/js/bootstrap.min.js";
+
+// axios
 import axios from "axios";
 
+// sweetalert
+import Swal from "sweetalert2";
+
+// api
 const url = "https://ec-course-api.hexschool.io/v2/api";
 const apiPath = "michaelhsia";
 
-// 代辦：
-// 1. 購物車刪除單一
-// 2. 查看更多
-// 3. 表單驗證
-// 4. 加入購物車跟查看更多按鈕榜loading
+// VeeValidate 方法載入
+const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
+const { required, email, min, max } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
 
-const app = createApp({
+// VeeValidate 驗證資料
+defineRule("required", required);
+defineRule("email", email);
+defineRule("min", min);
+defineRule("max", max);
+
+// VeeValidate 抓取語言檔案
+loadLocaleFromURL(
+  "https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json"
+);
+
+// 自定驗證語言跟形式
+configure({
+  generateMessage: localize("zh_TW"),
+  validateOnInput: true, // 輸入時驗證
+});
+
+const app = Vue.createApp({
   data() {
     return {
       // 完成商品資料
       products: [],
+
       // 購物車資料
       carts: [],
+
+      detailModal: null,
+      detailProduct: {},
+      // 查看更多的商品數量，不宣告該變數畫面仍可以動，但會跳黃字警告
+      qty: 1,
+
+      // 表單 post 資訊
+      form: {
+        user: {
+          name: "",
+          email: "",
+          tel: "",
+          address: "",
+        },
+        message: "",
+      },
     };
   },
+  components: {
+    VForm: Form,
+    VField: Field,
+    ErrorMessage: ErrorMessage,
+  },
   methods: {
+    openModal(product) {
+      this.detailModal.show();
+      this.detailProduct = product;
+    },
     getProductData() {
       axios
         .get(`${url}/${apiPath}/products/all`)
         .then((res) => {
           this.products = res.data.products;
         })
-        .catch((err) => alert(err.response));
+        .catch((err) => {
+          Swal.fire({
+            title: `${err.response.data.message}`,
+            icon: "warning",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        });
     },
-    addCartProduct(productId) {
+    addCartProduct(productId, qty = 1) {
       const data = {
         data: {
           product_id: productId,
-          qty: 1,
+          qty, // 等於 qty: qty
         },
       };
       axios
         .post(`${url}/${apiPath}/cart`, data)
         .then((res) => {
-          this.getCart();
+          // 呼叫載入效果
+          let loader = this.$loading.show();
+
+          this.detailModal.hide();
+
+          // loader 傳入 getCart，等取得購物車後把載入效果關閉
+          this.getCart(loader);
+
+          setTimeout(() => {
+            loader.hide();
+          }, 800);
+
+          // 彈出視窗效果
+          setTimeout(() => {
+            Swal.fire({
+              title: `${res.data.message}`,
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }, 1200);
         })
-        .catch((err) => alert(err.response));
+        .catch((err) => {
+          Swal.fire({
+            title: `${err.response.data.message}`,
+            icon: "warning",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        });
     },
     // 綁定 input change 事件
     updateCartProduct(cartId, productId, event) {
@@ -54,33 +145,119 @@ const app = createApp({
       axios
         .put(`${url}/${apiPath}/cart/${cartId}`, data)
         .then((res) => {
+          let loader = this.$loading.show();
+
           this.getCart();
+
+          setTimeout(() => {
+            loader.hide();
+          }, 800);
+
+          setTimeout(() => {
+            Swal.fire({
+              title: `${res.data.message}`,
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }, 1200);
         })
-        .catch((err) => alert(err.response));
+        .catch((err) => {
+          Swal.fire({
+            title: `${err.response.data.message}`,
+            icon: "warning",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        });
     },
     getCart() {
       axios
         .get(`${url}/${apiPath}/cart`)
         .then((res) => {
           this.carts = res.data.data.carts;
-          console.log(this.carts);
         })
-        .catch((err) => alert(err.response));
+        .catch((err) => {
+          Swal.fire({
+            title: `${err.response.data.message}`,
+            icon: "warning",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        });
     },
     delSingleCart(cartId) {
-      axios.delete(`${url}/${apiPath}/cart/${cartId}`).then((res) => {
-        this.getCart();
-        console.log(this.carts);
-      });
+      axios
+        .delete(`${url}/${apiPath}/cart/${cartId}`)
+        .then((res) => {
+          let loader = this.$loading.show();
+
+          this.getCart();
+
+          setTimeout(() => {
+            loader.hide();
+          }, 800);
+
+          setTimeout(() => {
+            Swal.fire({
+              title: `${res.data.message}`,
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }, 1200);
+        })
+        .catch((err) => {
+          Swal.fire({
+            title: `${err.response.data.message}`,
+            icon: "warning",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        });
     },
     delAllCart() {
       axios
         .delete(`${url}/${apiPath}/carts`)
         .then((res) => {
-          alert("已刪除所有購物車");
+          let loader = this.$loading.show();
+
           this.getCart();
+
+          setTimeout(() => {
+            loader.hide();
+          }, 800);
+
+          setTimeout(() => {
+            Swal.fire({
+              title: `${res.data.message}`,
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }, 1200);
         })
-        .catch((err) => alert(err.response));
+        .catch((err) => {
+          Swal.fire({
+            title: `${err.response.data.message}`,
+            icon: "warning",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        });
+    },
+    // 表單電話驗證
+    isPhone(value) {
+      const phoneNumber = /^(09)[0-9]{8}$/;
+      return phoneNumber.test(value) ? true : "需要正確的電話號碼";
+    },
+    submitOrder() {
+      axios
+        .post(`${url}/${apiPath}/order`, this.data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => alert(err.response.message));
     },
   },
   computed: {
@@ -92,9 +269,11 @@ const app = createApp({
     },
   },
   mounted() {
+    this.detailModal = new bootstrap.Modal(this.$refs.detailModal);
     this.getProductData();
     this.getCart();
   },
 });
 
+app.use(LoadingPlugin);
 app.mount("#app");
